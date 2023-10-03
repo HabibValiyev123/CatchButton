4507c532493d483ba42a67dce2480c86d0342ea0
1vali <1vali@DESKTOP-9EIRJHK>
2023-10-03 21:30:35 +0400

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            int MaxX = this.ClientSize.Width - button1.Width;
+            int MaxY = this.ClientSize.Height - button1.Height;
+            int newX = random.Next(MaxX);
+            int newY = random.Next(MaxY);
+
+            button1.Location = new Point(newX, newY);
+        }
+    }
+}
